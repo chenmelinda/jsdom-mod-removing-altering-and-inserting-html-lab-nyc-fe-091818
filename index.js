aeffd9c1a6a562e5924b39fdf065fdbb373b3db0
voluble-key-1260 <aiqiaochen21@gmail.com>
2018-10-17 00:22:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove()
+
 
 /* Create your new element here and assign it to newHeader */
+document.createElement('id') 
+newHeader.appendchild('id')
+
 const newHeader = null;
